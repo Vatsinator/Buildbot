e46009ddab04b79be6db3ae512360b545a3da22a
Michał Garapich <michal@garapich.pl>
2014-04-19 11:57:39 +0200

    devel

--- buildbot.py
+++ buildbot.py
@@ -10,40 +10,42 @@ from txutils import txupdate
 
 
 def read_config(file='config.ini'):
-  if not os.path.isfile(file):
-    print("Config file missing")
-    sys.exit(1)
-  
-  config = ConfigParser()
-  config.read(file)
-  return config
+    if not os.path.isfile(file):
+        print("Config file missing")
+        sys.exit(1)
+    
+    config = ConfigParser()
+    config.read(file)
+    return config
 
 
 def main():
-  '''
-  Parse command line options, read config and run desired action.
-  '''
-  description = "Vatsinator Buildbot automates server-side actions."
-  
-  parser = argparse.ArgumentParser(description=description)
-  parser.add_argument('command', choices=['txupdate'],
-                      help='command to be run', metavar='command')
-  parser.add_argument('--config', '-c', dest='configFile', default='config.ini',
-                      help='config file location (default: config.ini)',
-                      metavar='config_file')
-  
-  args = parser.parse_args()
-  
-  config = read_config(args.configFile)
-  
-  if args.command == 'txupdate':
-    if not config.has_section('Repository'):
-      print("Missing 'Repository' section in config file. See config.ini.sample for reference")
-      sys.exit(1)
+    '''
+    Parse command line options, read config and run desired action.
+    '''
+    description = "Vatsinator Buildbot automates server-side actions."
     
-    txupdate(repodir=config.get('Repository', 'vatsinator'),
-             txbranch=config.get('Repository', 'txbranch'))
-  
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('command', choices=['txupdate'],
+                        help='command to be run', metavar='command')
+    parser.add_argument('--config', '-c', dest='configFile', default='config.ini',
+                        help='config file location (default: config.ini)',
+                        metavar='config_file')
+    
+    args = parser.parse_args()
+    
+    config = read_config(args.configFile)
+    
+    if args.command == 'txupdate':
+        if not config.has_section('Repository'):
+            print("Missing 'Repository' section in config file. See config.ini.sample for reference")
+            sys.exit(1)
+        
+        txupdate(repodir=config.get('Repository', 'vatsinator'),
+                 txbranch=config.get('Translations', 'txbranch'),
+                 txgencmd=config.get('Translations', 'txgencmd')
+                 )
+
 
 if __name__ == "__main__":
   main()
--- txutils.py
+++ txutils.py
@@ -2,69 +2,97 @@
 # -*- coding: utf-8 -*-
 
 import git
+import os
 import sys
+from txclib import commands as tx
 
 '''
 Raised when local clone of the repository is not prepared to work with
 '''
 class RepoCloneDirtyError(Exception):
-  pass
+    pass
 
 '''
 Raised when the specified repo path does not exist
 '''
 class NoSuchPathError(Exception):
-  pass
+    pass
 
 
 def _get_repo(dir):
-  '''
-  Get reference to repo, check if its okay and on desired branch
-  '''
-  try:
-    repo = git.Repo(dir)
+    '''
+    Get reference to repo, check if its okay and on desired branch
+    '''
+    try:
+        repo = git.Repo(dir)
     
-    if repo.bare:
-      raise RepoCloneDirtyError("The repository is empty")
+        if repo.bare:
+            raise RepoCloneDirtyError("The repository is empty")
     
-    if repo.is_dirty():
-      raise RepoCloneDirtyError("Your local clone of the repository has uncommited changes")
-    return repo
+        if repo.is_dirty():
+            raise RepoCloneDirtyError("Your local clone of the repository has uncommited changes")
+        return repo
   
-  except git.NoSuchPathError as e:
-    raise NoSuchPathError("The specified path (%s) does not exist" % e)
+    except git.NoSuchPathError as e:
+        raise NoSuchPathError("The specified path (%s) does not exist" % e)
 
 
 def _repo_checkout_branch(repo, branch):
-  '''
-  Switch to the specified branch.
-  '''
-  myBranch = getattr(repo.heads, branch)
-  myBranch.checkout()
-  
+    '''
+    Switch to the specified branch.
+    '''
+    myBranch = getattr(repo.heads, branch)
+    myBranch.checkout()
+
 
 def _repo_pull(repo):
-  '''
-  git pull
-  '''
-  o = repo.remotes.origin
-  try:
-    o.pull()
-  except AssertionError:
     '''
-    There is this bug, still unresolved. But pull is successful.
+    git pull
     '''
-    pass
+    o = repo.remotes.origin
+    try:
+        o.pull()
+    except AssertionError:
+        '''
+        There is this bug, still unresolved. But pull is successful.
+        '''
+        pass
+
+
+def _tx_update_source(cmd, path):
+    '''
+    Calls cmd to generate new source translation and
+    pushes new source to transifex.
+    '''
+    os.system(cmd)
+    tx.cmd_push(['--source'], path)
+
+
+def _tx_pull(path):
+    '''
+    Pull translations from Transifex
+    '''
+    tx.cmd_pull(['--all', '--source', '--force'], path)
   
 
 def txupdate(**kwargs):
-  repoDir = kwargs['repodir']
-  txbranch = kwargs['txbranch']
-  
-  repo = _get_repo(repoDir)
-  
-  print("Checking out branch %s..." % txbranch)
-  _repo_checkout_branch(repo, txbranch)
-  
-  print("Pulling...")
-  _repo_pull(repo)
+    repoDir = kwargs['repodir']
+    txbranch = kwargs['txbranch']
+    txgencmd = kwargs['txgencmd']
+    
+    txgencmd = txgencmd.replace('%REPO%', repoDir)
+    
+    repo = _get_repo(repoDir)
+    
+    print("Checking out branch %s..." % txbranch)
+    _repo_checkout_branch(repo, txbranch)
+    
+    print("Pulling repository...")
+    _repo_pull(repo)
+
+    print("Updating source translation...")
+    _tx_update_source(txgencmd, repoDir)
+
+    print("Pulling translations...")
+    _tx_pull(repoDir)
+
